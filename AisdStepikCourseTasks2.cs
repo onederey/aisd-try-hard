99fb382196c49060c5303a25df55c48fd6810cba
0nederey <morozovcustom@yandex.ru>
2025-04-22 21:50:56 +0300

    заготовочка

---
+++ AisdStepikCourseTasks2.cs
@@ -0,0 +1,10 @@
+/// <summary>
+/// Задачи курса Алгоритмы и структуры данных. Методы (https://stepik.org/course/181477)
+/// </summary>
+public static class AisdStepikCourseTasks2
+{
+    public static void Cinema()
+    {
+        
+    }
+}
