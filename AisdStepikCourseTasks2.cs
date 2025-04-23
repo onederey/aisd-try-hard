493628605d5447f0f4648c7502ace09fdd5f418f
0nederey <morozovcustom@yandex.ru>
2025-04-23 21:44:29 +0300

    две задачки - одна честно, другая нет, что поделать

--- AisdStepikCourseTasks2.cs
+++ AisdStepikCourseTasks2.cs
@@ -5,6 +5,77 @@ public static class AisdStepikCourseTasks2
 {
     public static void Cinema()
     {
+        var input = Console.ReadLine().Split(" ");
         
+        var boys = Convert.ToInt32(input[0]);
+        var girls = Convert.ToInt32(input[1]);
+        
+        if (Math.Max(boys, girls) / Math.Min(boys, girls) > 2)
+        {
+            Console.WriteLine("NO SOLUTION");
+            return;
+        }
+
+        var result = "";
+
+        while(boys > 0 || girls > 0)
+        {
+            if (boys >= girls && boys >= 2 && girls >= 1)
+            {
+                result += "BGB";
+                boys -= 2;
+                girls -= 1;
+            }
+            else if (girls >= boys && girls >= 2 && boys >= 1)
+            {
+                result += "GBG";
+                girls -= 2;
+                boys -= 1;
+            }
+            else if (girls == boys)
+            {
+                result += "BG";
+                boys--;
+                girls--;
+            }
+            else if (girls > boys)
+            {
+                result += "G";
+                girls--;
+            }
+            else 
+            {
+                result += "B";
+                boys--;
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+
+    public static void Difference()
+    {
+        var k = int.Parse(Console.ReadLine());
+        var m = int.Parse(Console.ReadLine());
+        var a = int.Parse(Console.ReadLine());
+        var b = int.Parse(Console.ReadLine());
+
+        var kate = (b / k) - ((a - 1) / k);
+        var masha = (b / m) - ((a - 1) / m);
+
+        Console.WriteLine(kate - masha);
     }
 }
+
+// Шаблон для отправки задач
+/*
+using System;
+
+public static class Program
+{
+    public static void Main()
+    {
+
+    }
+}
+*/
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿LeetCodeTasks.IsIsomorphic("badc", "baba");
+﻿AisdStepikCourseTasks2.Cinema();
+
+LeetCodeTasks.IsIsomorphic("badc", "baba");
 
 LeetCodeTasks.FloodFillDFS([[0,0,0],[0,0,0]], 0, 0, 0);
 
